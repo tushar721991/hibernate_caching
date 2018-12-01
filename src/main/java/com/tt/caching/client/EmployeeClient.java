408552a6b8b69ce6faccc199487244ef1d51ddf1
unknown <thakurtusharm@gmail.com>
2018-12-01 16:31:12 +0530

    first commit

---
+++ src/main/java/com/tt/caching/client/EmployeeClient.java
@@ -0,0 +1,29 @@
+package com.tt.caching.client;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.tt.caching.model.Employee;
+import com.tt.caching.util.SessionFactoryProvider;
+
+public class EmployeeClient {
+	public static void main(String[] args) {
+
+		SessionFactory factory = SessionFactoryProvider.getSessionFactory();
+		Session session1 = factory.openSession();
+		Employee e1 = session1.get(Employee.class, 101);
+		Employee e2 = session1.get(Employee.class, 101);
+		System.out.println(e1.hashCode());
+		System.out.println(e2.hashCode());
+		session1.close();
+		
+		Session session2 = factory.openSession();
+		Employee e3 = session2.get(Employee.class, 101);
+		session2.close();
+		
+		
+		System.out.println(e3.hashCode());
+		factory.close();
+
+	}
+}
---
+++ src/main/java/com/tt/caching/util/SessionFactoryProvider.java
@@ -0,0 +1,27 @@
+package com.tt.caching.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.tt.caching.model.Employee;
+
+
+public class SessionFactoryProvider {
+
+	
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	static {
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure().build();
+		sessionFactory = new MetadataSources(registry)
+				.addAnnotatedClass(Employee.class)
+				.buildMetadata().buildSessionFactory();
+	}
+}
